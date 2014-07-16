3050dd9044caa4c9c6d40762e0b6de5ddaa3d5f5
Marcelo J. Both <marcelo.both@gmail.com>
2014-07-16 16:12:25 -0300

    Refactor and remove function

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions]
+  devise_for :users, skip: [ :sessions ]
   as :user do
     post '/api/login' => 'sessions#create'
     delete '/api/logout' => 'sessions#destroy'
   end
 
   namespace :api do
-    resources :edges, only: [:index]
-    resources :users, only: [:index]
+    resources :edges, only: [ :index, :create, :destroy, :update ]
+    resources :users, only: [ :index ]
+    resources :categories, only: [ :index ]
   end
 end
